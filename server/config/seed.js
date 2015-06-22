8551964e40d50d53b2084296ee90993277a5e7cb
Adrian Mejia <adriansky@gmail.com>
2015-06-22 16:28:04 -0400

    seeds

--- server/config/seed.js
+++ server/config/seed.js
@@ -5,28 +5,21 @@
 
 'use strict';
 
-var Thing = require('../api/thing/thing.model');
+var Product = require('../api/product/product.model');
 var User = require('../api/user/user.model');
 
-Thing.find({}).remove(function() {
-  Thing.create({
-    name : 'Development Tools',
-    info : 'Integration with popular tools such as Bower, Grunt, Karma, Mocha, JSHint, Node Inspector, Livereload, Protractor, Jade, Stylus, Sass, CoffeeScript, and Less.'
+Product.find({}).remove(function() {
+  var num = 0;
+  Product.create({
+    title : 'Product ' + (++num),
+    price : num * 100.33,
+    stock : num * 3,
+    description : 'This is the description of product ' + num
   }, {
-    name : 'Server and Client integration',
-    info : 'Built with a powerful and fun stack: MongoDB, Express, AngularJS, and Node.'
-  }, {
-    name : 'Smart Build System',
-    info : 'Build system ignores `spec` files, allowing you to keep tests alongside code. Automatic injection of scripts and styles into your index.html'
-  },  {
-    name : 'Modular Structure',
-    info : 'Best practice client and server structures allow for more code reusability and maximum scalability'
-  },  {
-    name : 'Optimized Build',
-    info : 'Build process packs up your templates as a single JavaScript payload, minifies your scripts/css/images, and rewrites asset names for caching.'
-  },{
-    name : 'Deployment Ready',
-    info : 'Easily deploy your app to Heroku or Openshift with the heroku and openshift subgenerators'
+    title : 'Product ' + (++num),
+    price : num * 100.33,
+    stock : num * 3,
+    description : 'This is the description of product ' + num
   });
 });
 
