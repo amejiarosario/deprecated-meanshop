65f9eec280ce3b66683a2a1ba0b7d8cef60c3098
Adrian Mejia <adriansky@gmail.com>
2015-07-28 19:18:34 -0400

    admin page improvements

--- client/app/admin/admin.controller.js
+++ client/app/admin/admin.controller.js
@@ -1,16 +1,34 @@
 'use strict';
 
 angular.module('meanstackApp')
-  .controller('AdminCtrl', function ($scope, $http, Auth, User) {
+  .controller('AdminCtrl', function ($scope, $http, $location, Auth, User, Products) {
 
     // Use the User $resource to fetch all users
     $scope.users = User.query();
+    $scope.products = Products.query();
 
-    $scope.delete = function(user) {
+    $scope.deleteUser = function(user) {
       User.remove({ id: user._id });
       angular.forEach($scope.users, function(u, i) {
         if (u === user) {
           $scope.users.splice(i, 1);
+        }
+      });
+    };
+
+    $scope.showProduct = function(product){
+      return $location.path('/products/' + product._id);
+    };
+
+    $scope.editProduct = function(product){
+      return $location.path('/products/' + product._id + '/edit');
+    };
+
+    $scope.deleteProduct = function(product){
+      Products.remove({ id: product._id });
+      angular.forEach($scope.products, function(p, i) {
+        if (p === product) {
+          $scope.products.splice(i, 1);
         }
       });
     };
