0f4c7106aaff80231fd75051af3c7408b19ee50d
Adrian Mejia <adriansky@gmail.com>
2015-08-01 12:30:21 -0400

    cleanup client/app/main and fixes unit tests

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -4,28 +4,9 @@ angular.module('meanstackApp')
   .controller('MainCtrl', function ($scope, $http, socket, Products) {
     Products.query(function (products) {
       $scope.products =  products.slice(0, 3);
-    });
-
-    $scope.awesomeThings = [];
-
-    $http.get('/api/things').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
-      socket.syncUpdates('thing', $scope.awesomeThings);
-    });
-
-    $scope.addThing = function() {
-      if($scope.newThing === '') {
-        return;
-      }
-      $http.post('/api/things', { name: $scope.newThing });
-      $scope.newThing = '';
-    };
-
-    $scope.deleteThing = function(thing) {
-      $http.delete('/api/things/' + thing._id);
-    };
-
-    $scope.$on('$destroy', function () {
-      socket.unsyncUpdates('thing');
+      // TODO: make sockets work on products updates
+      socket.syncUpdates('products', function (products) {
+        $scope.products =  products.slice(0, 3);
+      });
     });
   });
--- client/app/main/main.controller.spec.js
+++ client/app/main/main.controller.spec.js
@@ -7,23 +7,45 @@ describe('Controller: MainCtrl', function () {
   beforeEach(module('socketMock'));
 
   var MainCtrl,
-      scope,
+      $scope,
       $httpBackend;
+
+  var productsAttributes = [
+    {_id: 1, title: 'Product 1', price: 100.10 },
+    {_id: 2, title: 'Product 2', price: 200.20 },
+    {_id: 3, title: 'Product 3', price: 3100.10 },
+    {_id: 4, title: 'Product 4', price: 400.20 },
+    {_id: 5, title: 'Product 5', price: 500.10 },
+    {_id: 6, title: 'Product 6', price: 600.20 },
+  ];
 
   // Initialize the controller and a mock scope
   beforeEach(inject(function (_$httpBackend_, $controller, $rootScope) {
     $httpBackend = _$httpBackend_;
-    $httpBackend.expectGET('/api/things')
-      .respond(['HTML5 Boilerplate', 'AngularJS', 'Karma', 'Express']);
+    $httpBackend.expectGET('/api/products')
+      .respond(productsAttributes);
 
-    scope = $rootScope.$new();
+    $scope = $rootScope.$new();
     MainCtrl = $controller('MainCtrl', {
-      $scope: scope
+      $scope: $scope
     });
+
+    $httpBackend.flush();
   }));
 
-  it('should attach a list of things to the scope', function () {
-    $httpBackend.flush();
-    expect(scope.awesomeThings.length).toBe(4);
+  it('contains 3 products', function () {
+    expect($scope.products.length).toBe(3);
+  });
+
+  it('contains first 3 products', function() {
+    var ids = [];
+
+    angular.forEach($scope.products, function(product){
+      this.push(product._id);
+    }, ids);
+
+    expect(ids).toContain(1);
+    expect(ids).toContain(2);
+    expect(ids).toContain(3);
   });
 });
